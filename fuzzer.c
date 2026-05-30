eccbbbc224e36415c8e22974c23dc0534693903f
seanr06 <seanrothen06@gmail.com>
2026-05-30 14:15:59 -0400

    Finshed Grammer Fuzzer

--- fuzzer.c
+++ fuzzer.c
@@ -10,37 +10,31 @@ int main(int argc, char *argv[])
 {
     srand(time(NULL));
     bool crash = false;
-    time_t initialTime = time(NULL);
-    int numTests = 0;
+    time_t initial_Time = time(NULL);
+    int num_Tests = 0;
 
     // Loop until illegal argument is found
     while (crash == false)
     {
         char input[256] = "./target '";
-        // Fill input with 3 random byte characters
+        char valid_char[63] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        // Fill input with 3 random byte characters using grammer based fuzzing
         for (int i = 0; i < 3; i++)
         {
-            unsigned char byte = rand() % 256;
-            if (byte == 0x0 || byte == 0x0A || byte == 0x27)
-            {
-                i--;
-            }
-            else
-            {
-                char str[2] = {byte, '\0'};
-                strcat(input, str);
-            }
+            char byte = valid_char[rand() % 62];
+            char str[4] = {byte, '\0'};
+            strcat(input, str);
         }
         strcat(input, "'");
         int result = system(input);
-        numTests++;
+        num_Tests++;
         // Check if the input causes an error
         if (result != 0)
         {
             crash = true;
             time_t finalTime = time(NULL);
             printf("Crash detected with input: %s\nNumber of tests: %d\nTotal Time: %fs\n",
-                   input, numTests, difftime(finalTime, initialTime));
+                   input, num_Tests, difftime(finalTime, initial_Time));
         }
     }
     return 0;
